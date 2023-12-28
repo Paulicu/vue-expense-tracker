fc268cf1334fb538b2fe101da2bbbeea7f2c87b0
Paul Uifalean <uifalean.paul96@gmail.com>
2023-12-29 01:31:02 +0200

    Implement Balance Component

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,26 @@
-<script>
+<script setup>
     import Header from './components/Header.vue';
     import Balance from './components/Balance.vue';
     import IncomeExpenses from './components/IncomeExpenses.vue';
     import TransactionList from './components/TransactionList.vue';
     import AddTransaction from './components/AddTransaction.vue';
+
+    import { ref, computed } from 'vue';
+
+    const transactions = ref([
+        { id: 1, text: 'Flower', amount: -19.99 },
+        { id: 2, text: 'Salary', amount: 299.97 },
+        { id: 3, text: 'Book', amount: -10 },
+        { id: 4, text: 'Camera', amount: 150 },
+    ]);
+
+    const total = computed(() => {
+        return transactions.value.reduce((accumulator, transaction) => {
+            return accumulator + transaction.amount;
+        }, 0);
+    });
+
+    /*
     export default
     {
         components: 
@@ -15,15 +32,23 @@
             AddTransaction,
         }
     }
+    */
 </script>
 
 <template>
     <Header></Header>
 
     <div class="container">
-        <Balance></Balance>
+        <Balance :total="total">
+
+        </Balance>
+
         <IncomeExpenses></IncomeExpenses>
-        <TransactionList></TransactionList>
+
+        <TransactionList :transactions="transactions">
+
+        </TransactionList>
+        
         <AddTransaction></AddTransaction>
     </div>
 </template>
--- src/components/TransactionList.vue
+++ src/components/TransactionList.vue
@@ -1,7 +1,59 @@
+<script setup>
+    import { defineProps } from 'vue';
+
+    const props = defineProps({
+        transactions: {
+            type: Array,
+            required: true,
+        }
+    });
+    
+    /* ==================== COMPOSITION API *LONG* METHOD ====================
+    export default {
+        setup() {
+            const transactions = [
+                { id: 1, text: 'Flower', amount: -19.99 },
+                { id: 2, text: 'Salary', amount: 299.97 },
+                { id: 3, text: 'Book', amount: -10 },
+                { id: 4, text: 'Camera', amount: 150 },
+            ];
+
+            return {
+                transactions,
+            };
+        },
+    };
+    */
+
+    /* ==================== OPTIONS API METHOD ====================
+    export default {
+        data() {
+            return {
+                transactions: [
+                    { id: 1, text: 'Flower', amount: -19.99 },
+                    { id: 2, text: 'Salary', amount: 299.97 },
+                    { id: 3, text: 'Book', amount: -10 },
+                    { id: 4, text: 'Camera', amount: 150 },
+                ],
+            };
+        },
+    };
+    */
+</script>
+
 <template>
     <h3>History</h3>
 
     <ul id="list" class="list">
+        <li v-for="transaction in transactions" 
+            :key="transaction.id" 
+            :class="transaction.amount < 0 ? 'minus' : 'plus'"
+        >
+            {{ transaction.text }}
+            <span> ${{ transaction.amount }}</span>
+            <button class="delete-btn">x</button>
+        </li>
+       <!--
         <li class="minus">
             Cash <span>-$400</span><button class="delete-btn">x</button>
         </li>
@@ -9,5 +61,6 @@
         <li class="plus">
             Paycheck <span>$800</span><button class="delete-btn">x</button>
         </li> 
+       -->
     </ul>
 </template>
